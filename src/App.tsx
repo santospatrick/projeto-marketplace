c2583ef5f2e735a7c842a5efdeb2a177744362d0
Patrick Santos <patrickspteixeira@gmail.com>
2024-08-18 23:58:18 -0300

    adiciona axios e react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 import './global.css'
 import { router } from './routes'
+import { queryClient } from './providers/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
