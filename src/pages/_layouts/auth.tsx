bb100049259cde36d3426f7f13e3953d94c09761
Patrick Santos <patrickspteixeira@gmail.com>
2024-08-19 00:42:45 -0300

    adiciona layout para paginas deslogadas e components shadcn

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from 'react-router-dom'
+import logo from '@/assets/logo.svg'
+import background from '@/assets/background.png'
 
 function AuthLayout() {
   return (
-    <div>
-      Header (Authenticated)
-      <Outlet />
+    <div className="container mx-auto grid grid-cols-2 gap-2 py-10">
+      <aside>
+        <header>
+          <img src={logo} alt="logo 'marketplace, painel de vendedor'" />
+        </header>
+        <img src={background} alt="funcionalidades do dashboard" />
+      </aside>
+      <main>
+        <Outlet />
+      </main>
     </div>
   )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,40 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { ArrowRight02Icon } from 'hugeicons-react'
+
 function SignIn() {
-  return <div>SignIn</div>
+  return (
+    <Card className="rounded-[2rem] border-none px-[4.5rem] py-[5rem]">
+      <CardHeader>
+        <CardTitle>Acesse sua conta</CardTitle>
+        <CardDescription>
+          Informe seu e-mail e senha para entrar
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>Form de login</form>
+        <Button className="mt-2 w-full px-[1.25rem] h-[56px] text-lg justify-between bg-orange-base">
+          Acessar <ArrowRight02Icon />
+        </Button>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2">
+        <p>Ainda não tem uma conta?</p>
+        <Button
+          variant="outline"
+          className="mt-2 w-full px-[1.25rem] h-[56px] text-lg justify-between bg-transparent border-orange-base text-orange-base"
+        >
+          Cadastrar <ArrowRight02Icon />
+        </Button>
+      </CardFooter>
+    </Card>
+  )
 }
 
 export default SignIn
